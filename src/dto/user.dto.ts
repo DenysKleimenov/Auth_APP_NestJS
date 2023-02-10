3f1d0089a5db24caac7d5c282df0f2c316114891
Denys Kleimenov <denyskleimenovit@gmail.com>
2023-02-10 16:09:05 +0200

    Validation was added

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,6 +1,25 @@
+import { IsNotEmpty, MinLength, IsEmail, IsEnum } from 'class-validator';
+
+enum roleId {
+  DEFAULT = 1,
+  MANAGER = 2,
+}
+
 export class UserDto {
+  @IsNotEmpty()
   readonly username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
   readonly password: string;
+
+  @IsNotEmpty()
+  @IsEnum(roleId, {
+    message: 'role id must be either 1(Default) or 2(Manager)',
+  })
   readonly role_id: number;
 }
